23b5d1df960b8e8ea5cb39c102b803dd6e817dbf
Ahmed <ahmad.rizgar.rashid@gmail.com>
2021-03-18 19:03:32 +0100

    Setup shopping cart grid

--- client-app/src/Components/ShoppingCart/ShoppingCart.tsx
+++ client-app/src/Components/ShoppingCart/ShoppingCart.tsx
@@ -1,3 +1,4 @@
+import { Grid, Paper } from '@material-ui/core'
 import React from 'react'
 import { AppHeader } from '../../App/AppHeader'
 import { AppState } from '../../App/AppState'
@@ -7,7 +8,25 @@ export const Cart: React.FC<AppState> = (props: AppState) => {
     return (
         <>
             <AppHeader {...props.headerState} />
-            <p>SHOPPING CART COMING SOON</p>
+            <Grid container spacing={3}  direction="column"  >
+                <Grid item md={2}>
+                    <Paper>My card</Paper>
+                </Grid>
+                <Grid item md={2}>
+                    <Paper>Items</Paper>
+                </Grid>
+                <Grid item md={2}>
+                    <Paper>Subtotal</Paper>
+                </Grid>
+                <Grid item md={2}>
+                    <Paper>Somebull shit about shipping</Paper>
+                </Grid>
+                <Grid item md={2}>
+                    <Paper>Checkout</Paper>
+                </Grid>
+
+            </Grid>
+            
         </>
     )
 }
